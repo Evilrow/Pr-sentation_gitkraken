c6616e0cf0b8224d2cfdefb134e09599856869ca
Sébastien <sebastien.gaulier27@gmail.com>
2025-09-09 15:16:16 +0200

    test

--- plop.js
+++ plop.js
@@ -2,8 +2,12 @@ function plop(){
     return "plop"
 }
 
-function jvfbfdbor(ya, yo){
-    return "youpiiiiii"
+function josfgggrfr(ya, yo){
+    return "youpiiiii"
+}
+
+function tor(ya, yo){
+    return "youpiiiii"
 }
 
 function jouer(ya, yo){
